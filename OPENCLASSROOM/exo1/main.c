1f668d5eb1b3ca839c064e6156cbef727638ccfe
durandvictor <victor.durand@protonmail.com>
2020-04-11 14:15:05 +0200

    End of Exo 1

--- OPENCLASSROOM/exo1/main.c
+++ OPENCLASSROOM/exo1/main.c
@@ -1,13 +1,30 @@
-#include "STM32f10x.h"
-
-int main (void) 
-{
-    RCC->APB2ENR |=  RCC_APB2ENR_IOPAEN ;
-    GPIOA->CRL = GPIOA->CRL & ~(0xF << 4*5); 
-    GPIOA->CRL = GPIOA->CRL | (0x01 << 4*5);
-    while(1) 
+#include <STM32f10x.h>   
+ 
+char modulo;  
+ 
+void configure(){  
+    RCC->APB2ENR |=  RCC_APB2ENR_IOPAEN ;   
+    GPIOA->CRL = GPIOA->CRL & ~(0xF << 4*5);    
+    GPIOA->CRL = GPIOA->CRL | (0x01 << 4*5);  
+}  
+  
+void blink(){  
+    char cycle = 20;
+    modulo = modulo+1; 
+ 
+    if (modulo %cycle == 0) 
     { 
-        GPIOA->ODR ^= (1 << 5);
+        modulo = 0; 
+        GPIOA->ODR ^= (1 << 5);  
     } 
-    return 0; 
+}  
+  
+int main (void)   {   
+    configure();  
+    while(1)    
+    {    
+        blink();   
+    }    
+ 
+    return 0 ;   
 }
